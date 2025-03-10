6225ac41cdbb3e29f5f8a850da44224abf06de98
Chris <47590299+csmol26@users.noreply.github.com>
2025-03-10 13:32:11 +0100

    change card form

--- src/lib/primer.ts
+++ src/lib/primer.ts
@@ -66,11 +66,11 @@ export const initPrimer = async (config: PrimerCheckoutConfig): Promise<void> =>
             <p class="text-base font-medium text-gray-700 mb-4">Card - Display 1</p>
             <primer-card-form>
               <div slot="card-form-content">
-                <primer-input-card-holder-name placeholder="Christophe Smol"></primer-input-card-holder-name>
-                <primer-input-card-number placeholder="4444 3333 2222 1111"></primer-input-card-number>
-                <primer-input-card-expiry placeholder="12/30"></primer-input-card-expiry>
-                <primer-input-cvv placeholder="123"></primer-input-cvv>
-                <button type="submit">Pay Now</button>
+              <primer-input-card-number placeholder="4444 3333 2222 1111"></primer-input-card-number>
+              <primer-input-card-expiry placeholder="12/30"></primer-input-card-expiry>
+              <primer-input-cvv placeholder="123"></primer-input-cvv>
+              <primer-input-card-holder-name placeholder="Christophe Smol"></primer-input-card-holder-name>
+              <button type="submit">Pay Now with Card</button>
               </div>
             </primer-card-form>
             </primer-payment-method>
